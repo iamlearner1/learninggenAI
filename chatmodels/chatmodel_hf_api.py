0cb1423c013175dec3596131e47a18a4efadc36a
Jakka Shiva Kumar Reddy <jakkashivakumar222@gmail.com>
2025-09-26 12:46:25 +0530

    completed demo on using the open source models using hugging face api

---
+++ chatmodels/chatmodel_hf_api.py
@@ -0,0 +1,10 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="HuggingFaceH4/zephyr-7b-beta")
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India?")
+print(result.content)
